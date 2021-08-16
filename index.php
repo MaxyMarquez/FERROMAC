be61066e4e33f0d535d173bd90fbdcd024f9e8e8
MaxyMarquez <59151536+MaxyMarquez@users.noreply.github.com>
2021-08-16 19:27:57 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,39 +1,38 @@
-<?php
+<? php
+if (isset($_POST['submit'])) {
+    //ini_set( 'display_errors', 1 ); # REMOVE // FOR DEBUG
+    //error_reporting( E_ALL ); # REMOVE // FOR DEBUG
+    $from = "https://github.com/MaxyMarquez/FERROMAC.git"; // Email con el dominio del Hosting para evitar SPAM
+    $fromName = "name"; // Nombre que saldrá en el email recibido
+    $to = "maxy.marquez@hotmail.com"; // Dirección donde se enviará el formulario
+    $subject = $_POST['message']; // Asunto del Formulario
 
-if ($_POST['g-recaptcha-response'] == '') {
-echo "Captcha invalido";
-} else {
-$obj = new stdClass();
-$obj->secret = "6LeXlJ8UAAAAAB-jEG2M49toqrp0N_GgpApSdVTn";
-$obj->response = $_POST['g-recaptcha-response'];
-$obj->remoteip = $_SERVER['REMOTE_ADDR'];
-$url = 'https://www.google.com/recaptcha/api/siteverify';
+    /* Componemos el mensaje */
+    $fullMessage = "Hola! " . $to . "\r\n";
+    $fullMessage .= $_POST['name'] . " " . $_POST['lastName'] . " te ha enviado un mensaje:\r\n";
+    $fullMessage .= "\r\n";
+    $fullMessage .= "Nombre: " . $_POST['name'] . "\r\n";
+    $fullMessage .= "Apellidos: " . $_POST['lastName'] . "\r\n";
+    $fullMessage .= "E-Mail: " . $_POST['email'] . "\r\n";
+    $fullMessage .= "Mensaje: " . $_POST['message'] . "\r\n";
+    $fullMessage .= "\r\n";
+    $fullMessage .= "Saludos!\r\n";
 
-$options = array(
-'http' => array(
-'header' => "Content-type: application/x-www-form-urlencoded\r\n",
-'method' => 'POST',
-'content' => http_build_query($obj)
-)
-);
-$context = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
+    /* Creamos las cabeceras del Email */
+    $headers = "Organization: RPF WEB\r\n";
+    $headers .= "From: " . $fromName . "<" . $from . ">\r\n";
+    $headers .= "Reply-To: " . $_POST['email'] . "\r\n";
+    $headers .= "Return-Path: " . $to . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+    $headers .= "X-Priority: 3\r\n";
+    $headers .= "X-Mailer: PHP". phpversion() ."\r\n";
 
-$validar = json_decode($result);
-
-/*  FIN DE CAPTCHA   */
-
-if ($validar->success) {
-$email = trim($_POST['email']);
-$nombre = trim($_POST['name']);
-$apellido = trim($_POST['lastName']);
-$comentario = trim($_POST['message']);
-
-$consulta = "E-mail: " . $email . " Nombre: " . $nombre . " Apellido: " . $apellido . "Comentario: " . $comentario;
-
-mail("maxy.marquez@hotmail.com", "Contacto desde Formulario", $consulta);
-} else {
-echo "Captcha invalido";
-}
+    /* Enviamos el Formulario*/
+    if (mail($to, $subject, $fullMessage, $headers)) {
+        echo "<center><h2>El E-Mail se ha enviado correctamente!</h2></center>";
+    } else {
+        echo "<center><h2>Ops ! El E-Mail ha fallado!</h2></center>S";
+    }
 }
 ?>
