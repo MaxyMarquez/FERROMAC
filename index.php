fd882bc40b4fd4d1ea52dce31c1a17aad256c2ff
MaxyMarquez <59151536+MaxyMarquez@users.noreply.github.com>
2021-08-16 18:53:38 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+$nombre = $ _POST['name'];
+$email = $ _POST['email'];
+$mensaje = $ _POST['message'];
+$para = 'maxy.marquez@hotmail.com';
+$titulo = 'Hola - La Webera';
+ 
+$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
+ 
+if ($ _POST['submit']) {
+if (mail ($para, $titulo, $msjCorreo)) {
+echo 'El mensaje se ha enviado';
+} else {
+echo 'Falló el envio';
+}
+}
